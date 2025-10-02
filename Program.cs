b3b2cf24f17b183d19a2b2417c4989d0e31c417f
ProgramerPlus <hipitelo@outlook.com.com>
2025-10-02 20:24:30 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿﻿Console.WriteLine("Milhas para Km");
+Console.Write("Informe o valor em milhas: ");
+string entrada = Console.ReadLine()!;
+double umaMilhaEmKm = 1.609;
+double milhas = Convert.ToDouble(entrada);
+double resultado = umaMilhaEmKm * milhas;
+Console.WriteLine($"{resultado} km");
